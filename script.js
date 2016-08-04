7bbbe1e9cbf61577c2c7d24484aa87b172066dc2
henrylatham <henry@henrylatham.me>
2016-08-04 12:03:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
 //$('.button').someAction for a class//
-
 $(document).ready(function() {
-   $('#tagline').mouseenter(function() {
-       $(this).animate({
-           height: '+=10px'
-       });
-   });
-   $('#tagline').mouseleave(function() {
-       $(this).animate({
-           height: '-=10px'
-       }); 
-   });
-   $('#tagline').click(function() {
-       $(this).toggle(1000);
-   }); 
+    $().fadeOut(1000);
 });
