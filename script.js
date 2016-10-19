c062506bafc03c68e0f6add5a586241f62693c50
henrylatham <henry@henrylatham.me>
2016-10-19 17:21:38 -0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
 
-$(document).ready(function() {
-
-    $('.caption').hover(function(){
-        $('.caption').fadeOut();
-    });
-
-    $('.read-more').click(function() {
-        $('.read-more').animate({left:'+=20px'},100);
-    });
-    
-    
-    
-});
-
